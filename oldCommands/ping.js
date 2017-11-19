01253c407abfdce5712c5c43bc4a9008c39284c6
william schreiber <blogboywill@gmail.com>
2017-11-19 00:20:16 -0500

    Removed Unnessicary files

--- oldCommands/ping.js
+++
@@ -1,9 +0,0 @@
-exports.run = async (client, message, args) => {
-
-
-  const msg = await message.channel.send("ping");
-  msg.edit(`Pong! \`${msg.createdTimestamp - message.createdTimestamp}\`ms`);
-}
-module.exports.config = {
-  command: "ping"
-}
