5b10da134d2481ef4dac26efa47d73f28ddd7450
william schreiber <blogboywill@gmail.com>
2017-11-24 09:44:03 -0500

    Added extra message when debug mode is on

--- commands/credits.js
+++ commands/credits.js
@@ -1,9 +1,13 @@
 exports.run = (client, message, args) => {
     const author = message.author.id;
+    const settings = client.settings.get(message.guild.id);
 
     const credits = client.credits.get(author) || {credits: 0};
 
     message.channel.send(`${message.author} you have ${credits.credits} credits`);
+    if(settings.debug == true){
+    message.channel.send(JSON.stringify(credits));
+    }
 }
 exports.conf = {
     enabled: true,
