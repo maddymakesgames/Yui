c30daf83849b0fdcc17bae18a90c212787d9a52b
william schreiber <blogboywill@gmail.com>
2017-11-30 19:45:05 -0500

    edited the ExampleConfig file

--- ExampleConfig.js
+++ ExampleConfig.js
@@ -1,13 +1,11 @@
-
 const config = {
-
-  
   // Bot Owner, level 10 by default. A User ID. Should never be anything else than the bot owner's ID.
-  "ownerID": "Your ID Here!",
+  "ownerID": "Your User ID Here",
 
   //Bot Contributors, level 10 by default. These people are given acess to debug commands.
   //When self hosting you probably don't need this unless you are changing code.
-  "contributors":[],
+  "contributors": [],
+
   // Bot Admins, level 9 by default. Array of user ID strings.
   "admins": [],
 
@@ -15,22 +13,27 @@ const config = {
   "support": [],
 
   // Your Bot's Token. Available on https://discordapp.com/developers/applications/me
-  "token": "Your Bot Token here!",
+  "token": "Your Token Here",
+
+  'defaultProfile': {
+    'tagline': 'Insert Witty Quote Here',
+    'description': 'A person is me'
+  },
 
   // Default per-server settings. New guilds have these settings.
 
   // DO NOT LEAVE ANY OF THESE BLANK, AS YOU WILL NOT BE ABLE TO UPDATE THEM
   // VIA COMMANDS IN THE GUILD.
 
-  "defaultSettings" : {
+  "defaultSettings": {
     "prefix": ["y!", "Y!"], //The prefixes used. Must be an array. It's good to, if you are using a letter, to have both a capital and a lowercase version of your prefix.
     "modLogChannel": "mod-log",
     "modRole": "Moderator",
     "adminRole": "Administrator",
     "systemNotice": "true", // This gives a notice when a user tries to run a command that they do not have permission to use.
-    "GeneralChannel":"general",
-    "AnnouncementsChannel":"announcements",
-    "BotAnnouncementsEnabled":"true",
+    "GeneralChannel": "general",
+    "AnnouncementsChannel": "announcements",
+    "BotAnnouncementsEnabled": "true",
     "welcomeChannel": "welcome",
     "welcomeMessage": "Say hello to {{user}}!",
     "welcomeEnabled": "true",
@@ -39,13 +42,12 @@ const config = {
     "debug": "false" //Whether or not the debug mode is active. LEAVE THIS AS FALSE AS A DEFAULT! This, if set to true, might not be able to be turned off in the server.
   },
 
-  "defaultServerScore": {},
-
   // PERMISSION LEVEL DEFINITIONS.
 
   permLevels: [
     // This is the lowest permisison level, this is for non-roled users.
-    { level: 0,
+    {
+      level: 0,
       name: "User",
       // Don't bother checking, just return true which allows them to execute any command their
       // level allows them to.
@@ -53,7 +55,8 @@ const config = {
     },
 
     // This is your permission level, the staff levels should always be above the rest of the roles.
-    { level: 2,
+    {
+      level: 2,
       // This is the name of the role.
       name: "Moderator",
       // The following lines check the guild the message came from for the roles.
@@ -70,7 +73,8 @@ const config = {
       }
     },
 
-    { level: 3,
+    {
+      level: 3,
       name: "Administrator",
       check: (message) => {
         try {
@@ -82,7 +86,8 @@ const config = {
       }
     },
     // This is the server owner.
-    { level: 4,
+    {
+      level: 4,
       name: "Server Owner",
       // Simple check, if the guild owner id matches the message author's ID, then it will return true.
       // Otherwise it will return false.
@@ -90,8 +95,9 @@ const config = {
     },
 
     // Bot Support is a special inbetween level that has the equivalent of server owner access
-    // to any server they join, in order to help troubleshoot the bot on behalf of owners and contributors.
-    { level: 8,
+    // to any server they join, in order to help troubleshoot the bot on behalf of owners.
+    {
+      level: 8,
       name: "Bot Support",
       // The check is by reading if an ID is part of this array. Yes, this means you need to
       // change this and reboot the bot to add a support user. Make it better yourself!
@@ -99,7 +105,8 @@ const config = {
     },
 
     // Bot Admin has some limited access like rebooting the bot or reloading commands.
-    { level: 9,
+    {
+      level: 9,
       name: "Bot Admin",
       check: (message) => config.admins.includes(message.author.id)
     },
@@ -110,9 +117,12 @@ const config = {
       name: "Contributors",
       check: (message) => config.contributors.includes(message.author.id)
     },
+
     // This is the bot owner, this should be the highest permission level available.
-    // Can use commands that could be dangerous. Only useful if eval or exce is added. Otherwise pretty much unused.
-    { level: 11,
+    // The reason this should be the highest level is because of dangerous commands such as eval
+    // or exec (if the owner has that).
+    {
+      level: 11,
       name: "Bot Owner",
       // Another simple check, compares the message author id to the one stored in the config file.
       check: (message) => message.client.config.ownerID === message.author.id
