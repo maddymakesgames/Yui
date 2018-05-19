0900105dc61444d3e5da3440bc9b464ebbe34fc6
willikesgames <blogboywill@gmail.com>
2018-05-19 00:06:02 -0400

    Update guildCreate.js
    Removed DBL api stuff because having your token there is a bad idea.

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -1,19 +1,7 @@
 // This event executes when a new guild (server) is joined.
 
 module.exports = (client, guild) => {
-	const dblApi = require('discord-bot-list');
-	const dbl = new dblApi({
-		'token': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjM2NzE1MTE2ODg2Mzk5Mzg1NiIsImJvdCI6dHJ1ZSwiaWF0IjoxNTE1Mjg1NzkyfQ.ZptBrQnLCWqkkE9lN8f7GC0x71_kN1yRpDD8ssVpK9g',
-		'id': '367151168863993856',
-	});
-	dbl.postStats(client.guilds.size, (err, res) => {
-		if (err) {
-			console.error(err);
-		}
-		else {
-			console.log(res);
-		}
-	});
+	
 
 	client.user.setPresence({ status: 'online', game: { name: `${client.guilds.size}` } });
 	// We need to add this guild to our settings!
